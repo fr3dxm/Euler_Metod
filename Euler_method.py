4f8fb6b78d4ba9841c7b4b2e455a4eb35cddf22f
fr3dxm <138373059+fr3dxm@users.noreply.github.com>
2024-06-08 12:01:38 +0300

    Update Euler_method.py

--- Euler_method.py
+++ Euler_method.py
@@ -1,43 +1,36 @@
-#include <stdio.h>
-#include <math.h>
+import numpy as np
 
-//                                               
-void analytical_solution(double t, double* x1, double* x2) {
-    *x1 = 0.5 - (499.0 / 999.0) * exp(-t) - (1.0 / 1998.0) * exp(-1000.0 * t);
-    *x2 = 0.5 + (500.0 / 999.0) * exp(-t) - (1.0 / 1998.0) * exp(-1000.0 * t);
-}
+def euler_method(t0, x0, h, tk):
+    n = int((tk - t0) / h)
+    x = np.zeros((n+1, 2))
+    x[0] = x0
+    for i in range(n):
+        x1 = x[i, 0]
+        x2 = x[i, 1]
+        x[i+1, 0] = x1 + h * (-501 * x1 + 499 * x2 + 1)
+        x[i+1, 1] = x2 + h * (500 * x1 - 500 * x2)
+    return x
 
-void euler_method(double t, double h, double* x1, double* x2) {
-    int num_steps = (int)(t / h) + 1;
-    double x1_numeric, x2_numeric;
-    double t_val = 0.0;
-    int k = 0.1 / h;
+# задаем начальные условия
+t0 = 0
+x0 = np.array([0, 0])
+h = 0.05
+tk = 1.0
 
-    for (int i = 0; i < num_steps; i++) {
-        t_val = i * h;
+# решаем систему методом Эйлера
+x_euler = euler_method(t0, x0, h, tk)
 
-        if (i % k == 0) {
-            printf("%.1f %.5f %.5f", t_val, *x1, *x2);
-            analytical_solution(t_val, &x1_numeric, &x2_numeric);
-            printf(" %.5f %.5f\n", x1_numeric, x2_numeric);
-}
+# печатаем таблицу значений
+print("Метод Эйлера:")
+print("t\tx1\tx2")
+for i in range(0, len(x_euler)):
+    print("{:.2f}\t{:.4f}\t{:.4f}".format(i*h, x_euler[i, 0], x_euler[i, 1]))
 
- double x1_dot = -501.0 * *x1 + 499.0 * *x2 + 1.0;
- double x2_dot = 500.0 * *x1 - 500.0 * *x2;
-
-*x1 += h * x1_dot;
-*x2 += h * x2_dot;
-}
-}
-
-int main() {
-    double x1_analytical = 0, x2_analytical = 0;
-    double t = 1.0;
-    double h = 0.001;
-
- //                      
-euler_method(t, h, &x1_analytical, &x2_analytical);
-printf("t_val\teiler_1\teiler_2\tanal_1\tanal_2\n");
-
-return 0;
-}
+# печатаем таблицу значений аналитического решения
+print("Аналитическое решение:")
+print("t\tx1\tx2")
+for i in range(0, int(tk/h)+1):
+    t = i * h
+    x1 = 0.5 + 0.5*np.exp(-t) - 1000*t
+    x2 = 0.5 - 0.5*np.exp(-t)
+    print("{:.2f}\t{:.4f}\t{:.4f}".format(t, x1, x2))
